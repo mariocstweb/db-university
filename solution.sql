a56c1c8410fac8dd573a9e355610b8e665bfeba7
mariocstweb <mariocstmra@gmail.com>
2024-03-01 17:09:26 +0100

    added 5 solution

--- solution.sql
+++ solution.sql
@@ -16,6 +16,10 @@ WHERE `period` = 'I semestre'
 AND `year` = 1
 --  5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 --  20/06/2020 (21)
+SELECT * 
+FROM `exams`
+WHERE DATE(`date`) = '2020-06-20'
+AND TIME(`hour`) > '14:00:00';
 --  6. Selezionare tutti i corsi di laurea magistrale (38)
 --  7. Da quanti dipartimenti è composta l'università? (12)
 --  8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
