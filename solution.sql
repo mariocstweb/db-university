918990debf35421988819edf6206fe449f00c2bc
mariocstweb <mariocstmra@gmail.com>
2024-03-01 17:10:48 +0100

    added 6 solution

--- solution.sql
+++ solution.sql
@@ -21,6 +21,9 @@ FROM `exams`
 WHERE DATE(`date`) = '2020-06-20'
 AND TIME(`hour`) > '14:00:00';
 --  6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = 'magistrale';
 --  7. Da quanti dipartimenti è composta l'università? (12)
 --  8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
